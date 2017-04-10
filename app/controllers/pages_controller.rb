37146063155e303bf5083542e1ba5e366718d98e
Zachary Viggiani <zach@bigmojo.net>
2017-04-10 15:47:36 -0400

    contest redirect setup

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,4 +8,20 @@ class PagesController < ApplicationController
   def welcome
     @header = "This is the what I am putting in the H1 tag"
   end
+def contest 
+  redirect_to "/welcome"
+end
+
+  def kitten
+    set_kitten_url
+  end
+
+  def kittens
+    set_kitten_url
+  end
+
+  def set_kitten_url
+    requested_size = params[:size]
+    @kitten_url = "http://lorempixel.com/#{requested_size}/#{requested_size}/cats"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/welcome' => 'pages#welcome'
-  get '/' => 'pages#welcome'
+  get '/kitten/:size' => 'pages#kitten'
+
+  get '/contest' => 'pages#contest'
+
 end
